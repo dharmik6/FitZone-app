16f9fe4d516815888f1c2ee3a49aa73cdd58f109
dharmik6 <dharmik.kacha.2526@outlook.com>
2023-10-03 14:55:47 +0530

    complete user data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,9 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+    buildFeatures{
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,6 +39,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -51,4 +55,6 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     // Also add the dependency for the Google Play services library and specify its version
     implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/example/fitzone/profile_page.java
+++ app/src/main/java/com/example/fitzone/profile_page.java
@@ -1,14 +1,79 @@
 package com.example.fitzone;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class profile_page extends AppCompatActivity {
+
+    private ImageView profileImageView;
+    private TextView userNameTextView, userAgeTextView, userGenderTextView, userHeightTextView, userWeightTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        profileImageView = findViewById(R.id.profile_img);
+        userNameTextView = findViewById(R.id.user_name);
+        userAgeTextView = findViewById(R.id.user_age);
+        userGenderTextView = findViewById(R.id.user_gender);
+        userHeightTextView = findViewById(R.id.user_height);
+        userWeightTextView = findViewById(R.id.user_weight);
+
+        ProgressDialog pd = new ProgressDialog(this);
+        pd.setMessage("Loading user data");
+        pd.setCancelable(false);
+        pd.show();
+// Retrieve and set the user's name, email, and profile image from Firebase Realtime Database
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        // Assuming `userId` is the unique identifier for the user
+        String userId = currentUser.getUid();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(userId);
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Retrieve user data
+                    String name = dataSnapshot.child("name").getValue(String.class);
+                    int age = dataSnapshot.child("age").getValue(Integer.class);
+                    String gender = dataSnapshot.child("gender").getValue(String.class);
+                    int height = dataSnapshot.child("height").getValue(Integer.class);
+                    int weight = dataSnapshot.child("weight").getValue(Integer.class);
+                    String imageUrl = dataSnapshot.child("img").getValue(String.class);
+
+                    // Set user data to UI elements
+                    userNameTextView.setText(name);
+                    userAgeTextView.setText(String.valueOf(age));
+                    userGenderTextView.setText(gender);
+                    userHeightTextView.setText(String.valueOf(height));
+                    userWeightTextView.setText(String.valueOf(weight));
+
+                    // Load user profile image into ImageView using Glide
+                    Glide.with(profile_page.this).load(imageUrl).into(profileImageView);
+                }
+
+                // Dismiss the progress dialog after loading data
+                pd.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle database errors
+                pd.dismiss();
+            }
+        });
     }
 }
