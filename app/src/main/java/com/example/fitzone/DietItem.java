958291cc5399a8884b44db9c92a03ba06ca0bd6d
Dhruv <dhruvpandav04@gmail.com>
2023-10-04 22:48:19 +0530

    diet

--- app/src/main/java/com/example/fitzone/DietItem.java
+++ app/src/main/java/com/example/fitzone/DietItem.java
@@ -1,24 +1,31 @@
 package com.example.fitzone;
 
 public class DietItem {
+
     private String dietName;
     private String dietDescription;
     private String imageUrl;
 
-    public DietItem(String dietName, String dietDescription, String imageUrl) {
-        this.dietName = dietName;
-        this.dietDescription = dietDescription;
-        this.imageUrl = imageUrl;
-    }
-    public DietItem(String dietName, String imageUrl) {
-        this.dietName = dietName;
-        this.imageUrl = imageUrl;
-    }
     public String getDietName() {
         return dietName;
     }
 
+    public String getDietDescription() {
+        return dietDescription;
+    }
+
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public DietItem(String dietName, String imageUrl) {
+        this.dietName = dietName;
+        this.imageUrl = imageUrl;
+    }
+
+    public DietItem(String dietName, String dietDescription, String imageUrl) {
+        this.dietName = dietName;
+        this.imageUrl = imageUrl;
+        this.dietDescription = dietDescription;
     }
 }
